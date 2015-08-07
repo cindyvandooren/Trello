da017ffac54ff8e73973ba8be750fb54b313da3a
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-08-07 13:28:51 -0700

    Add json.jbuilder templates for Board, fix bugs and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Board.create!({title: "Board 1", user_id: 2})
+Board.create!({title: "Board 2", user_id: 2})
+Board.create!({title: "Board 3", user_id: 2})
+Board.create!({title: "Board 4", user_id: 2})
+Board.create!({title: "Board 5", user_id: 2})
+Board.create!({title: "Board 6", user_id: 2})
+Board.create!({title: "Funny Board", user_id: 1})
+Board.create!({title: "Lame Board", user_id: 1})
+Board.create!({title: "Happy Board", user_id: 1})
+Board.create!({title: "Sad Board", user_id: 1})
